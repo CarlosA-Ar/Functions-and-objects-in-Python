1a8ea6a80858b55e49f7e922f5225c0d51901256
Carlos.a.Ar <carlos.ar@ciencias.unam.mx>
2022-01-03 22:56:00 -0600

    Obtener la estructura del código así como los archivos fuente del proyecto

--- ANALISIS02_AVILA_CARLOS.py
+++ ANALISIS02_AVILA_CARLOS.py
@@ -1,7 +1,37 @@
 """
-Proyecto 02
+Proyecto 02. Análisis de las opciones que conformarían la base para crear la 
+estrategia operativa de "Synergy Logistics".
 
 Carlos Avila Argüello
 EmTech
+
+Recomendación a quien leerá el código: plegar las pestañas (si usas Spyder)
+para el mejor entendimiento. La estructura del código está básicamente dividida
+en tres partes, cada una contiene el código para obtener resultados que nos faciliten
+decidir qué opción(opciones) es(son) mejor(es).
 """
+#%% Importar los datos:
+
+
+#%% Opción 1. Enfque en las rutas
+# Obtener una tabla (.csv) que contiene:
+    # Nombre de la ruta que se recorre (país origen -> país destino).
+    # Número de veces que se ha recorrido esa ruta.
+    # Monto acumulado en esa ruta.
+
+
+#%% Opción 2. Enfoque en los transportes
+# Obtener una tabla (.csv) que contiene:
+    # Nombre del transporte usado.
+    # Número de veces que se ha usado ese transporte.
+    # Monto acumulado que se ha obtenido con el uso de ese transporte.
+
+
+#%% Opción 3. Enfoque en los países
+# Obtener dos tablas (.csv´s), ambas contienen:
+    # Nombre del país.
+    # Número de veces que se repite ese país en la base.
+    # Monto que se emitió por país.
+# Las tablas serán diferentes porque una se enfocará en el país que importa
+# y la otra en el país que exporta.
 
